661c0fead124b08dc38d2c0e9ec8b54c559e4a9b
Rick Liu <rickliu2000@outlook.com>
2017-04-05 19:57:59 +0800

    materialize

---
+++ center/service_outside_io.php
@@ -0,0 +1,98 @@
+<?php require "components/Head.php" ?>
+
+   <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="clearfix"></div>
+
+            <div class="row">
+                
+			  
+			  
+			  <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2><i class="fa fa-info"></i> 说明 </h2>
+					
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+					<p>此处列举的服务与nfls.io（下面简称“本站”）无任何直接关系，相关服务均由第三方组织提供，本站不承担使用以下服务间接或直接产生的任何问题。</p>
+                    <div class="clearfix"></div>
+
+                  </div>
+                </div>
+              </div>
+			  
+			  <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2><i class="fa fa-globe"></i> Shadowsocks <small>ss.oscs.io</small></h2>
+					
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+					<p>状态：已激活</p>
+                    <div class="clearfix"></div>
+
+                  </div>
+                </div>
+              </div>
+			  
+			  <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2><i class="fa fa-book"></i> 教务系统 <small>jw.nfls.com.cn</small></h2>
+					
+                    <ul class="nav navbar-right panel_toolbox">
+					<li></li><li></li>
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                     
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+					<p>状态：死机中</p>
+                    <div class="clearfix"></div>
+
+                  </div>
+                </div>
+              </div>
+			  
+	
+			  
+			  
+			</div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+	<script src="//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+	<script src="head.js"></script>
+  </body>
+</html>
