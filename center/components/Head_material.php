f5f3a6a0142a591833f27d2c27d0a8d5236f8a3b
Rick Liu <rickliu2000@outlook.com>
2017-04-05 20:15:58 +0800

    materialized some part of login

---
+++ center/components/Head_material.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>NFLS.IO 用户中心</title>
+
+     <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
+       <link href="../materialize/css/prism.css" rel="stylesheet">
+    <link href="../materialize/css/ghpages-materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    
+    <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="../materialize/js/materialize.min.js"></script>
+    </body>
+
+    <!-- PNotify -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-92193639-8', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <!-- Custom Theme Style -->
+
+<!--
+<?php // require "parts/Navigation_material.php";?> 
+-->
+  </head>
+<body>
+<header>
+   <nav class="top-nav blue">
+        <div class="container">
+          <div class="nav-wrapper blue"><a class="page-title">Account</a>
+        </div>
+      </nav>
+      <div class="container">
+      <a href="#" data-activates="nav-mobile" class="button-collapse top-nav full">
+      <i class="material-icons">menu</i>
+      </a>
+      </div>
+      <ul id="nav-mobile" class="side-nav fixed">
+        <li><div class="userView blue">
+         <img class="circle" id="avatar"></a>
+        <span class="white-text name" id="user_name"></span></a>
+        <span class="white-text email">tianzundibei@163.com</span></a>
+    </div></li>
+        <li class="no-padding">
+          <ul class="collapsible collapsible-accordion">
+            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Personal Settings</a>
+              <div class="collapsible-body">
+                <ul>
+                  <li><a href="personal_info_material.php">Personal information</a></li>
+                  <li><a href="service@io.php">Service @ nfls.io</a></li>
+                  <li><a href="service_outsite_io">nothing about this site =_= </a></li>
+                </ul>
+              </div>
+            </li>
+            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Messages</a>
+              <div class="collapsible-body">
+                <ul>
+                  <li><a href="badges.html">System Messages</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+          <a class="waves-effect" href="https://nfls.io/quickaction.php?action=logout&noreturn=true"><span class="black-text">log out</span></a>
+        </li>
+
+      </li>
+    </ul>
+  </div>
+</header>
+</body>
+ <script>
+// Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+   $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  </script> 
+
+  
+
+  <?php require "parts/Footer_material.php"; ?>
---
+++ center/components/parts/Navigation_material.php
@@ -0,0 +1,109 @@
+<?php //require_once "/var/www/login.nfls.io/function/UserControl.php";?>
+        <!-- Dropdown Structure -->
+<header>
+<nav class="top-nav">
+ <div class="container">
+  <div class="nav-wrapper">
+    <ul class="right hide-on-med-and-down">
+       <!--<li><img id ="avatar_2" alt=""></li>-->
+       <li><a href="https://nfls.io/quickaction.php?action=logout&noreturn=true">Logout</a></li>
+      <!-- Dropdown Trigger -->
+      
+    </ul>
+  </div>
+</div>
+</nav>
+</header>
+        
+
+        <!-- top navigation 
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav class="" role="navigation">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img id ="avatar_2" alt="">
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="https://nfls.io/quickaction.php?action=logout&noreturn=true" target="_parent"><i class="fa fa-sign-out pull-right"></i> 退出</a></li>
+                  </ul>
+                </li>
+      
+                <li role="presentation" class="dropdown">
+                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>
+                  </a>
+                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="text-center">
+                        <a>
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+        -->
+              </ul>
+            </nav>
+          </div>
+        </div>
+<script>$(".dropdown-button").dropdown();</script>
+        <!-- /top navigation -->
+    
