70e5e1ae24936984d6dfa6c34d0dc08f8461f631
Rick Liu <rickliu2000@outlook.com>
2017-04-05 19:55:50 +0800

    head.js ported for material design style pages

---
+++ center/head_material.js
@@ -0,0 +1,106 @@
+if($.cookie("token")==null||$.cookie("flarum_remember")==null)
+	logout();
+$.ajax({
+	type: "POST",
+	url: "https://app.nfls.io/API/User/User.php?action=GetAvatarByToken",
+	data: 
+	{
+		token: $.cookie('token'),
+	},
+	dataType: "json",
+	success: function (message) {
+		//var json_mes=$.parseJSON(message)
+		if(message.status=="success")
+		{
+			//alert(message);
+			document.getElementById("avatar").src = message.url; 
+			document.getElementById("avatar_2").src = message.url; 
+		}
+		else
+		{
+			logout();
+		}
+			
+	},
+});
+
+$.ajax({
+	type: "POST",
+	url: "https://app.nfls.io/API/User/User.php?action=GetUsernameByToken",
+	data: 
+	{
+		token: $.cookie('token'),
+	},
+	dataType: "json",
+	success: function (message) {
+		//var json_mes=$.parseJSON(message)
+		if(message.status=="success")
+		{
+			//alert(message);
+			document.getElementById("user_name").innerHTML = message.username; 
+		}
+			
+	},
+});
+
+	$.ajax({
+		type: "POST",
+		url: "https://app.nfls.io/API/User/User.php?action=GetPersonalWikiInfoByToken",
+		data: 
+		{
+			token: $.cookie('token'),
+		},
+		dataType: "json",
+		success: function (message) {
+			//var json_mes=$.parseJSON(message)
+			if(message.status=="success")
+			{
+				if(message.id!=-1)
+				{
+					if($.cookie("nfls_wiki_wiki_Token")==null||$.cookie("nfls_wiki_wiki_UserID")==null||$.cookie("nfls_wiki_wiki_UserName")==null)
+						logout();
+				}
+			}
+			else
+			{
+
+			}
+				
+		},
+		error: function (message) {
+		}
+	});
+
+	$.ajax({
+		type: "POST",
+		url: "https://app.nfls.io/API/User/User.php?action=GetPersonalShareInfoByToken",
+		data: 
+		{
+			token: $.cookie('token'),
+		},
+		dataType: "json",
+		success: function (message) {
+			if(message.status=="success")
+			{
+				if(message.id!=-1)
+				{
+					if($.cookie("c_secure_login")==null||$.cookie("c_secure_pass")==null||$.cookie("c_secure_ssl")==null||$.cookie("c_secure_tracker_ssl")==null)
+						logout();
+				}
+			}
+			else
+			{
+
+			}
+				
+		},
+		error: function (message) {
+		}
+	});
+
+function logout()
+{
+	//$.cookie('token', '', {path: '/', domain: 'nfls.io', secure: true, expires: -1});
+	window.location.href='https://nfls.io/quickaction.php?action=logout&noreturn=true';
+	//alert("!1");
+}
