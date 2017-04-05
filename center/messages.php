ae6b6c0f387e6d2c9566a26a258b1d901f7e8bbc
Rick Liu <rickliu2000@outlook.com>
2017-04-05 22:24:56 +0800

    message materialized

---
+++ center/messages.php
@@ -0,0 +1,92 @@
+<?php require "components/Head_material.php" ?>
+
+        <!-- page content -->
+<div class="container">
+ <!--
+        <div class="container">
+          <div class="row">
+      <div class="col s12 m9">        
+-->
+<div class="container">
+  <!--  Outer row  -->
+  <div class="row">
+
+    <div class="section col s12 m9 l10">
+            <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Messages</span>
+              <p>Check here to find latest updates about this server and school events</p>
+            </div>
+            <div class="card-action">
+              <a href="https://blog.oscs.io">Maintainer's blog</a>
+              
+            </div>
+          </div>
+          <ul class="collection" id="system_noticebox">
+    
+    </ul>
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+             <!-- <div class="x_panel" >
+                <div class="x_title">
+                  <h2>系统通知与公告 <small>仅显示最近10条</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content" >
+                  <ul class="list-unstyled timeline" id="system_noticebox">
+                  </ul>
+
+                </div>
+              </div>
+            </div> -->
+          </div>
+        </div>
+        <!-- /page content -->
+      </div>
+    </div>
+
+    <div id="custom_notifications" class="custom-notifications dsp_none">
+      <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+      </ul>
+      <div class="clearfix"></div>
+      <div id="notif-group" class="tabbed_notifications"></div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <!-- PNotify -->
+    <script src="../vendors/pnotify/dist/pnotify.js"></script>
+    <script src="../vendors/pnotify/dist/pnotify.buttons.js"></script>
+    <script src="../vendors/pnotify/dist/pnotify.nonblock.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+	<script src="//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+	<script src="head_material.js"></script>
+	<script src="messages.js"></script>
+
+
+  </body>
+</html>
