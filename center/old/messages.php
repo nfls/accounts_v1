3c9d79ad8f7a5df3f4fb522a3e8ca4be0cbc95a8
mmlmml1 <huqingyang2000@Gmail.com>
2017-04-06 13:16:41 +0800

    Add user auth

--- center/old/messages.php
+++ center/old/messages.php
@@ -1,4 +1,77 @@
-<?php require "components/Head_material.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NFLS.IO 用户中心 | User Center</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link type="text/css" rel="stylesheet" href="../bower_components/materialize/dist/css/materialize.min.css"
+          media="screen,projection"/>
+    <script type="text/javascript" src="../bower_components/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="../bower_components/materialize/dist/js/materialize.min.js"></script>
+    <script src="../bower_components/jquery.cookie/jquery.cookie.js"></script>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-92193639-8', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+<body>
+<header>
+    <nav class="top-nav blue">
+        <div class="container">
+            <div class="nav-wrapper blue"><a class="page-title">Account</a>
+                <a href="#" data-activates="nav-mobile" class="button-collapse top-nav full hide-on-large-only">
+                    <i class="material-icons">menu</i></a>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+    </div>
+    <ul id="nav-mobile" class="side-nav fixed">
+        <li>
+            <div class="userView blue">
+                <img class="circle" id="avatar"></a>
+                <span class="white-text name" id="user_name"></span></a>
+                <span class="white-text email">tianzundibei@163.com</span></a>
+            </div>
+        </li>
+        <li><a class="waves-effect" href="personal_info.html">账户综合信息</a></li>
+        <li><a class="waves-effect" href="associated_service.html">站内关联服务</a></li>
+        <li><a class="waves-effect" href="messages.html">消息与通知</a></li>
+        <li><a class="waves-effect" href="user_identity.html">实名认证</a></li>
+        <li><a class="waves-effect" href="https://nfls.io/quickaction.php?action=logout&noreturn=true">退出</a></li>
+    </ul>
+    </nav>
+</header>
+</body>
+<script>
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );
+</script>
 
         <!-- page content -->
 <div class="container">
