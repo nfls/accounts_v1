e600643b92d69d557c14b433b705f481bd353e4e
mmlmml1 <huqingyang2000@gmail.com>
2017-04-08 19:39:48 +0800

    Add support for showing basic info

--- center/js/user_identity.js
+++ center/js/user_identity.js
@@ -1,17 +1,20 @@
 /**
  * Created by hqy on 2017/4/8.
  */
-$url="http://v1.dev";
-/*
+var url="http://v1.dev";
 $.ajax({
     type: "GET",
-    url: $url + "/alumni/auth/getCurrentStep",
+    url: url + "/alumni/auth/getCurrentStep",
     dataType: "json",
+    xhrFields: {
+        withCredentials: true
+    },
+    crossDomain: true,
     success: function (message) {
         //var json_mes=$.parseJSON(message)
         if(message.code==200)
         {
-            goto(message.step);
+            gotoStep(message.step);
         }
         else
         {
@@ -23,12 +26,40 @@ $.ajax({
         alert("aaa");
     }
 });
-*/
-gotoStep(1);
-function gotoStep($step) {
-    switch($step){
+function queryInfo(step) {
+    $.ajax({
+        type: "GET",
+        url: url + "/alumni/auth/"+ step + "/query",
+        dataType: "json",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        success: function (message) {
+            //var json_mes=$.parseJSON(message)
+            if(message.code==200)
+            {
+                $('#username').val(message.nickname);
+                $('#email').val(message.email);
+                console.log(message);
+                return message;
+            }
+            else
+            {
+                alert("aaa");
+            }
+
+        },
+        error: function (message) {
+            alert("aaa");
+        }
+    });
+}
+function gotoStep(step) {
+    switch(step){
         case 1:
             $('#step1').show(2000);
+            queryInfo(1);
             break;
         case 2:
             $('#step2').show(2000);
