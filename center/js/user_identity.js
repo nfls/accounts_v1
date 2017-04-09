e5f0c4c6d6363ecbca1cf6e9538c4ad72ccdc58e
mmlmml1 <huqingyang2000@gmail.com>
2017-04-09 10:32:07 +0800

    More…

--- center/js/user_identity.js
+++ center/js/user_identity.js
@@ -1,7 +1,7 @@
 /**
  * Created by hqy on 2017/4/8.
  */
-var url="http://v1.dev";
+var url = "http://v1.dev";
 $.ajax({
     type: "GET",
     url: url + "/alumni/auth/getCurrentStep",
@@ -12,8 +12,7 @@ $.ajax({
     crossDomain: true,
     success: function (message) {
         showMessage(message.message);
-        if(message.code==200)
-        {
+        if (message.code == 200) {
             gotoStep(message.step);
         }
 
@@ -25,7 +24,7 @@ $.ajax({
 function queryInfo(step) {
     $.ajax({
         type: "GET",
-        url: url + "/alumni/auth/"+ step + "/query",
+        url: url + "/alumni/auth/" + step + "/query",
         dataType: "json",
         xhrFields: {
             withCredentials: true
@@ -33,7 +32,7 @@ function queryInfo(step) {
         crossDomain: true,
         success: function (message) {
             showMessage(message.message);
-            if(message.code==200)
+            if (message.code == 200)
                 updateForm(message.info);
         },
         error: function (message) {
@@ -41,10 +40,49 @@ function queryInfo(step) {
         }
     });
 }
+function submitInfo(step){
+    var formInfo = new Array;
+    $.each($('input', '#form'),function(k){
+        formInfo[$(this).attr('id')]=$(this).val()
+    });
+    $.each($('select', '#form'),function(k){
+        formInfo[$(this).attr('id')]=$(this).find("option:selected").attr("value");
+    });
+    formInfo.remove("undefined");
+    console.log(formInfo);
+    //console.log("")
+    /*
+    $("#form").each(
+        function(index,element) {
+            console.log(index);
+            console.log(element);
+        }
+    );
+    */
+    /*
+    $.ajax({
+        type: "GET",
+        url: url + "/alumni/auth/" + step + "/update",
+        dataType: "json",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        success: function (message) {
+            showMessage(message.message);
+            if (message.code == 200)
+                updateForm(message.info);
+        },
+        error: function (message) {
+            serverError();
+        }
+    });
+    */
+}
 function gotoStep(step) {
-    $("#previous").attr("disabled","disabled");
-    if(step>=1 && step<=4){
-        $('#step'+step).show(2000);
+    $("#previous").attr("disabled", "disabled");
+    if (step >= 1 && step <= 4) {
+        $('#step' + step).show(2000);
         queryInfo(step);
     }
     return 0;
@@ -52,14 +90,12 @@ function gotoStep(step) {
 function updateForm(message) {
     console.log(1);
     $.each(message,
-        function(index,element)
-        {
-            $('#'+index).val(element);
+        function (index, element) {
+            $('#' + index).val(element);
         }
     );
 }
-function serverError()
-{
+function serverError() {
     showMessage("与服务器通讯出现故障，请检查您的网络或是刷新重试。如果错误反复出现，请与网站管理员联系");
 }
 
