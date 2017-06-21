0347c6212c631155c56a82ec36fa36f285a24a5c
mmlmml1 <huqingyang2000@gmail.com>
2017-06-21 10:43:00 +0800

    Fix if error

--- operation/js/login.js
+++ operation/js/login.js
@@ -31,9 +31,9 @@ $('.login').on('submit', function(e) {
 						$this.addClass('ok');
 						$state.html('Welcome back!');
 						setTimeout(function () { 
-							if(returnurl=="")
+							//if(returnurl=="")
 								//window.location.href="https://nfls.io/quickaction.php?action=refreshwiki";
-							else
+							//else
 								//window.location.href="https://nfls.io/quickaction.php?action=refreshwiki&return="+encodeURI(returnurl);
 						}, 1000);
 				}
