39e110c38b9c7c2d662994cc5a07e7c9c619f3ba
mmlmml1 <huqingyang2000@gmail.com>
2017-07-13 16:42:13 -0400

    .

--- operation/login.php
+++ operation/login.php
@@ -217,7 +217,7 @@ footer a, footer a:link {
     <i class="fa fa-user"></i>
     <input type="password" placeholder="密码" id="password" name="password"/>
     <i class="fa fa-key"></i>
-    <img id="captcha" max-width="100%"></img>
+    <img id="captcha" width="100%"></img>
     <input type="password" placeholder="验证码" id="captcha" name="captcha"/>
     <i class="fa fa-flask"></i>
 	<input type="hidden" id="returnurl" value=<?php 
