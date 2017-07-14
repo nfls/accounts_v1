6a1f50b17540d7416661c176c5bea40f6e17ad23
mmlmml1 <huqingyang2000@gmail.com>
2017-07-13 22:56:10 -0400

    Change register process

--- operation/js/register.js
+++ operation/js/register.js
@@ -1,60 +1,72 @@
 var working = false;
-$('.login').on('submit', function(e) {
-	  var pass = document.getElementById("password").value;
-	  var user = document.getElementById("username").value;
-	  var passchk= document.getElementById("password-repeat").value;
-	  var email=document.getElementById("email").value;
-	  e.preventDefault();
-	  if (pass!=passchk)
-	  {
-	  	window.alert("Passowrd not match");
-	  	return;
-	  }
-	  if (working) return;
-	  working = true;
-	  var $this = $(this),
-		$state = $this.find('button > .state');
-	  $this.addClass('loading');
-	  $state.html('Registering');
-		$.ajax({
-			type: "POST",
-			url: "https://api.nfls.io/center/register",
-			data: 
-			{
-				username: user,
-				password: pass,
-				email: email,
-			},
-			dataType: "json",
-			success: function (message) {
-				if(message.status=="succeed")
-				{
-					$this.addClass('ok');
-					$state.html('Register Succeeded! Please check the confirm letter in you email! You may need to login again!');
-					setTimeout(function () { 
-							window.location.href="https://forum.nfls.io";
-					}, 5000);
-					
-				}
-				else
-				{
-					$this.addClass('error');
-					$state.html(message.message);
-					setTimeout(function () { 
-						window.location.href="index.php?action=register";
-					}, 5000);
+var session = "";
+loadCaptcha();
+function loadCaptcha() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.nfls.io/center/registerCaptcha",
+        success: function (message) {
+            document.getElementById('captcha').setAttribute('src', message["info"]["captcha"]);
+            session = message["info"]["session"];
+        }
+    })
+}
 
-				}
-				
-			},
-			error: function (message) {
-				$state.html('Log in');
-				$this.removeClass('ok loading');
-				working = false;
-				alert("请求错误，请稍后再试！");
-				window.location.href="index.php";
-			}
-		});
+$('.login').on('submit', function (e) {
+    var pass = document.getElementById("password").value;
+    var user = document.getElementById("username").value;
+    var passchk = document.getElementById("password-repeat").value;
+    var email = document.getElementById("email").value;
+    var captcha = document.getElementById("captcha_text").value;
+    e.preventDefault();
+    if (pass != passchk) {
+        window.alert("密码不一致!");
+        return;
+    }
+    if (working) return;
+    working = true;
+    var $this = $(this),
+        $state = $this.find('button > .state');
+    $this.addClass('loading');
+    $state.html('Registering');
+    $.ajax({
+        type: "POST",
+        url: "https://api.nfls.io/center/register",
+        data: {
+            username: user,
+            password: pass,
+            email: email,
+            session: session,
+            captcha: captcha
+        },
+        dataType: "json",
+        success: function (message) {
+            if (message.info.status == "succeed") {
+                $this.addClass('ok');
+                $state.html('注册成功！请检查您邮箱内的确认邮件。');
+                setTimeout(function () {
+                    window.location.href = "https://center.nfls.io";
+                }, 5000);
 
-      
+            }
+            else {
+                $this.addClass('error');
+                $state.html(message.info.message);
+                setTimeout(function () {
+                    window.location.href = "index.php?action=register";
+                }, 5000);
+
+            }
+
+        },
+        error: function (message) {
+            $state.html('Log in');
+            $this.removeClass('ok loading');
+            working = false;
+            alert("请求错误，请稍后再试！");
+            window.location.href = "index.php";
+        }
+    });
+
+
 });
--- operation/register.php
+++ operation/register.php
@@ -205,6 +205,9 @@ footer a, footer a:link {
     <i class="fa fa-key"></i>
     <input type="password" placeholder="确认密码" id="password-repeat" name="password-repeat"/>
     <i class="fa fa-key"></i>
+    <img id="captcha" width="100%"></img>
+    <input type="text" placeholder="验证码" id="captcha_text" name="captcha"/>
+    <i class="fa fa-flask"></i>
 
    <a href="index.php">登录</a> <br/>
    <a href="?action=privacy">隐私信息</a>
