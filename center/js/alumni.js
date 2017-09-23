bafbbff48c58b9a34d0782e23a2f31c13fcdb75c
mmlmml1 <huqingyang2000@gmail.com>
2017-09-23 22:46:15 +0800

    。

--- center/js/alumni.js
+++ center/js/alumni.js
@@ -101,7 +101,7 @@ function submitInfo(action) {
         formInfo[$(this).attr('id')] = getData_($(this).val());
     });
     $.each($('select', '#form' + step), function (k) {
-        if ($(this).attr('id') != null && $(this).is(":visible"))
+        if ($(this).attr('id') != null && ($(this).attr('id') !="hide")
             formInfo[$(this).attr('id')] = getData_($(this).find('option:selected').attr('value'));
     });
     var jsonData = JSON.stringify(formInfo);
